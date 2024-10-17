89caa67f1b96dd8b3645656b280ac3c96fb668af
Smit Tankariya <93311155+Smit028@users.noreply.github.com>
2024-10-17 12:05:14 +0530

    Initial commit

--- app/tem/page.js
+++ app/tem/page.js
@@ -14,6 +14,20 @@ function page() {
     
     </div>
     <ReportPage/>
+
+    
+
+<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://ahujacareerinstitute.com/" class="hover:underline">Ahuja Career Institue™</a>. All Rights Reserved.
+    </span>
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">created by: Smit Tankariya | <a href="mailto:sbtankariya@gamail.com">sbtankariya@gamail.com</a>
+
+    </span>
+
+    </div>
+</footer>
+
     </>
   )
 }
